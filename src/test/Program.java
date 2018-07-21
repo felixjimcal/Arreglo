a3eb442bde90c4b355a936baa6fc22cc2456b912
Felix JC <felixjcalvo@gmail.com>
2018-07-21 16:47:05 +0200

    Signed-off-by: Felix JC <felixjcalvo@gmail.com>

--- src/test/Program.java
+++ src/test/Program.java
@@ -3,6 +3,17 @@ package test;
 public class Program {
 	static boolean Insert(int number)
 	{
-		return true;
+		boolean pass = false;
+		
+		if(number >= 20)
+		{
+			pass = true;
+		}
+		else
+		{
+			pass = false;
+		}
+		
+		return pass;
 	}
 }
--- src/test/TestProgram.java
+++ src/test/TestProgram.java
@@ -7,12 +7,21 @@ import org.junit.Test;
 public class TestProgram {
 
 	@Test
-	public void Give20OrBiggerPass() 
+	public void Give20OrBigger() 
 	{
 		int value = 22;
 		boolean expected = true;
 		
 		assertEquals(expected, Program.Insert(20));
+		assertEquals(expected, Program.Insert(value));
+	}
+	
+	@Test
+	public void GiveLowerThan20() 
+	{
+		int value = 19;
+		boolean expected = false;
+		
 		assertEquals(expected, Program.Insert(value));
 	}
 
