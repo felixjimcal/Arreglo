d5ff45cb7b933b66e0ae9967dd0cd1c0f2398a9c
Felix JC <felixjcalvo@gmail.com>
2018-07-23 18:23:58 +0200

    Signed-off-by: Felix JC <felixjcalvo@gmail.com>

--- src/test/Program.java
+++ src/test/Program.java
@@ -1,7 +1,13 @@
 package test;
 
+import java.util.Vector;
+
 public class Program {
-	static boolean Insert(int number)
+	
+	private int current_value = 0;
+	private int[] nums = new int[5];
+	
+	boolean CheckValue(int number)
 	{
 		boolean pass = false;
 		
@@ -16,4 +22,16 @@ public class Program {
 		
 		return pass;
 	}
+	
+	public void InsertValue(int value)
+	{
+		nums[current_value] = value;
+		current_value++;
+	}
+	
+	public int[] ShowList()
+	{
+		return nums;
+	}
+	
 }
--- src/test/TestProgram.java
+++ src/test/TestProgram.java
@@ -2,9 +2,17 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Vector;
+
 import org.junit.Test;
 
 public class TestProgram {
+	
+	Program pro;
+	
+	public TestProgram() {
+		pro = new Program();
+	}
 
 	@Test
 	public void Give20OrBigger() 
@@ -12,8 +20,8 @@ public class TestProgram {
 		int value = 22;
 		boolean expected = true;
 		
-		assertEquals(expected, Program.Insert(20));
-		assertEquals(expected, Program.Insert(value));
+		assertEquals(expected, pro.CheckValue(20));
+		assertEquals(expected, pro.CheckValue(value));
 	}
 	
 	@Test
@@ -22,7 +30,21 @@ public class TestProgram {
 		int value = 19;
 		boolean expected = false;
 		
-		assertEquals(expected, Program.Insert(value));
+		assertEquals(expected, pro.CheckValue(value));
 	}
+	
+	@Test
+	public void ListIsFull() 
+	{
+		int nums[] = {26, 62, 73, 37, 45};
+		
+		pro.InsertValue(26);
+		pro.InsertValue(62);
+		pro.InsertValue(73);
+		pro.InsertValue(37);
+		pro.InsertValue(45);
 
+		assertArrayEquals(nums, pro.ShowList());
+	}
+	
 }
